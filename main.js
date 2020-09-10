c63e284532f6f48775df88e48d8acdd01c5eb82c
georgelewi5 <65343203+georgelewi5@users.noreply.github.com>
2020-09-10 16:03:26 +0100

    getting rid of alert

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 window.addEventListener("load", function () {
     const loader = document.querySelector(".loadercontainer");
     loader.className += " hidden"; // class "loader hidden"
-    alert(getComputedStyle(document.body).marginTop);
 });
-let script = document.createElement('html');
-html.src = "portfolio.html";
-document.head.append(script);
